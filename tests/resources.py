6d0bc9519616ce4d2b99ef89d9585db0a11d6016
Thibault Clérice <leponteineptique@gmail.com>
2016-05-10 09:44:07 -0400

    Refactored tests

--- tests/resources.py
+++ tests/resources.py
@@ -3,6 +3,7 @@ from flask.ext.nemo import Nemo
 from flask import Markup, Flask
 from capitains_nautilus.mycapytain import NautilusRetriever
 
+
 def create_test_app(debug=False, config=None):
     app = Flask(__name__)
     app.debug = debug
@@ -38,21 +39,21 @@ class NemoResource(unittest.TestCase):
     """ Test Suite for Nemo
     """
     endpoint = "http://website.com/cts/api"
-    body_xsl = "testing_data/xsl_test.xml"
+    body_xsl = "tests/test_data/xsl_test.xml"
 
     def setUp(self):
-        with open("testing_data/getcapabilities.xml", "r") as f:
+        with open("tests/test_data/getcapabilities.xml", "r") as f:
             self.getCapabilities = RequestPatch(f)
 
-        with open("testing_data/getvalidreff.xml", "r") as f:
+        with open("tests/test_data/getvalidreff.xml", "r") as f:
             self.getValidReff_single = RequestPatch(f)
             self.getValidReff = RequestPatchChained([self.getCapabilities, self.getValidReff_single])
 
-        with open("testing_data/getpassage.xml", "r") as f:
+        with open("tests/test_data/getpassage.xml", "r") as f:
             self.getPassage = RequestPatch(f)
             self.getPassage_Capabilities = RequestPatchChained([self.getCapabilities, self.getPassage])
 
-        with open("testing_data/getprevnext.xml", "r") as f:
+        with open("tests/test_data/getprevnext.xml", "r") as f:
             self.getPrevNext = RequestPatch(f)
             self.getPassage_Route = RequestPatchChained([self.getCapabilities, self.getPassage, self.getPrevNext])
 
